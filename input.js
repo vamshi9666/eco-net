c0e80f8203e0298dac9d4537e5072d6d65323a63
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-04-09 07:39:35 +0530

    able to change theme dynamcally . but have to search for best alg

--- input.js
+++ input.js
@@ -1,35 +1,69 @@
+console.log("back is running");
+browser.runtime.onMessage.addListener(listener);
 
 
-browser.runtime.onMessage.addListener(listener)
+var hundred = {
+  colors: {
+   accentcolor: '#76FF03',
+   textcolor: '#111'
+ }
+}; 
 
-
-
-
-function listener(message) {
-var  timeRes = message;
-console.log(timeRes)  
-let currentTheme = ' ';
-  console.log(currentTheme)
-  var currentTheme = '';
-
-  let themes = {
-    'healthy':{
-      colors: {
-        accentcolor: '#8BC34A',
-       textcolor: '#111',
-     }
-    },
-    'night': {
-      
-      colors: {
-        accentcolor: '#ef5350',
-        textcolor: '#fff',
-      }
-    }
-  };
-
-  browser.theme.update('healthy')
-
+var seventy = {
+  colors:{
+    accentcolor:'#CDDC39',
+    textcolor:'#111111'
+  }
 }
+var fifty = {
+  colors:{
+    accentcolor:'#CDDC39',
+    textcolor:'#111'
+  } 
+}
+var thirty = {
+  colors:{
+    accentcolor:'#FF8F00',
+    textcolor:'#111'
+  }
+}
+
+var ten = {
+  colors:{
+    accentcolor:'#E65100',
+    textcolor:'#111'
+  }
+}
+var zero = {
+  colors:{
+    accentcolor:'#f44336',
+    textcolor:'#111'
+  }
+}
+function listener(message) {
+    var  timeRes = message;
+    console.log(timeRes);  
+    if(timeRes < 3){
+      browser.theme.update(hundred)
+    }
+    else if(timeRes < 5) {
+      browser.theme.update(seventy)
+    }
+    else if (timeRes < 8){
+      browser.theme.update(fifty)
+    }
+    else if (timeRes < 10){
+      browser.theme.update(thirty)
+    }
+    else if (timeRes < 15){
+      browser.theme.update(ten)
+    }
+    else{
+      browser.theme.update(zero)
+    }
+}
+
+
+
 
 
